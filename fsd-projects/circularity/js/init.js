f5e3782ea2b9f465c67dbf1c13cc35f833dea34c
DylanMcBride5549939 <dmm5549939@lpssonline.com>
2026-03-30 18:54:01 +0000

    todo 9 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -69,19 +69,13 @@ drawCircle()
                physikz.updatePosition(circles[i]);
 
                game.checkCirclePosition(circles[i]);
-               
+
 }
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
            game.checkCirclePosition(circles[ 0 ])
 
-game.checkCirclePosition(circles[ 1 ])
 
-game.checkCirclePosition(circles[ 2 ])
-
-game.checkCirclePosition(circles[ 3 ])
-
-game.checkCirclePosition(circles[ 4 ])
 
             // TODO 8 / TODO 9 : Iterate over the array
            
